4fd421348746d4cc9f57009c4c7e2693ed37c834
Ivan Lugo <ivan.lugo@sightplan.com>
2014-09-21 13:27:49 -0400

    Forcing a merge conflict.

--- FastPassParking/FastPassParking/ViewController.m
+++ FastPassParking/FastPassParking/ViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    NSLog(@"test");
+    NSLog(@"Ivan overwrote the test.");
     
 }
 
