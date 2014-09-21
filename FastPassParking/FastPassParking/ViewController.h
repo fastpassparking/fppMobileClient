27b63405297a830f440c60416376c67f556b8598
Ivan Lugo <ivan.lugo@sightplan.com>
2014-09-21 13:00:01 -0400

    Initial Project Commit.

---
+++ FastPassParking/FastPassParking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FastPassParking
+//
+//  Created by Ivan Lugo on 9/21/14.
+//  Copyright (c) 2014 FastPassParking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FastPassParking/FastPassParking/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FastPassParking
+//
+//  Created by Ivan Lugo on 9/21/14.
+//  Copyright (c) 2014 FastPassParking. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
