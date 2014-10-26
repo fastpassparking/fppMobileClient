f61713b035bc511616fa6612e0267ecf07c2926d
Alex Gordon <Alex@Macintosh.local>
2014-10-26 13:50:00 -0400

    New User_Information class

---
+++ FastPassParking/FastPassParking/User_Information.h
@@ -0,0 +1,13 @@
+//
+//  User_Information.h
+//  FastPassParking
+//
+//  Created by Alex Gordon on 10/26/14.
+//  Copyright (c) 2014 FastPassParking. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface User_Information : UIViewController
+
+@end
---
+++ FastPassParking/FastPassParking/User_Information.m
@@ -0,0 +1,37 @@
+//
+//  User_Information.m
+//  FastPassParking
+//
+//  Created by Alex Gordon on 10/26/14.
+//  Copyright (c) 2014 FastPassParking. All rights reserved.
+//
+
+#import "User_Information.h"
+
+@interface User_Information ()
+
+@end
+
+@implementation User_Information
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
