b2f2bb4d1d811a2b41a425370283c2921e1c18c4
Ivan Lugo <ivan.lugo@sightplan.com>
2014-10-29 17:55:27 -0400

    Initial user push to back end

--- FastPassParking/FastPassParking/ViewController.m
+++ FastPassParking/FastPassParking/ViewController.m
@@ -23,6 +23,39 @@
     test_height.text = [NSString stringWithFormat:@"%d",height2];
     
     
+    NSURL* url = [NSURL URLWithString:[@"http://107.203.220.120" stringByAppendingPathComponent:@"User"]];
+    NSMutableDictionary* userWrapper = [[NSMutableDictionary alloc] init];
+    NSMutableDictionary* newUser = [[NSMutableDictionary alloc] init];
+    NSString* email = @"thisisanotheremail@stuff.com";
+    NSString* password = @"asupersecurepasswordAGAINBITCH";
+//    NSString* arbitraryData = @"84uq9wdjisauhdih23874h273he7qhwd7qw6hre";
+    [newUser setObject:email forKey:@"email"];
+    [newUser setObject:password forKey:@"password"];
+//    [newUser setObject:arbitraryData forKey:@"arbitraryStuff"];
+    [userWrapper setObject:newUser forKey:@"user"];
+    
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:userWrapper
+                                                       options:NSJSONWritingPrettyPrinted
+                                                         error:NULL];
+    
+    NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:url];
+    request.HTTPMethod = @"POST";
+    [request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    NSURLSessionConfiguration* config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession* session = [NSURLSession sessionWithConfiguration:config];
+    
+    request.HTTPBody = jsonData;
+    
+    NSURLSessionDataTask* task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        
+        NSLog(@"Finished. user is:");
+        NSString* stringFromData = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"%@", stringFromData);
+    }];
+    
+    [task resume];
+    
 }
 
 - (void)didReceiveMemoryWarning {
