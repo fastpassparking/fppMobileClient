b8c66ce3959ade0e636761f4ad8aff857a414133
kyleMera <kmera1@knights.ucf.edu>
2014-11-02 17:57:30 -0500

    Added request methods
    Added request methods for:
    getParkingLotsForBoundingBox
    createParkingPass

---
+++ FastPassParking/FastPassParking/ParkingLotHandler.h
@@ -0,0 +1,17 @@
+//
+//  ParkingLotHandler.h
+//  FastPassParking
+//
+//  Created by Kerl on 11/2/14.
+//  Copyright (c) 2014 FastPassParking. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ParkingLotHandler : NSObject
+
++(void) getParkingLotsForBoundingBox:(NSNumber*) minLat withMaxLat:(NSNumber*) maxLat
+                         withMinLong:(NSNumber*) minLong withMaxLong:(NSNumber*) maxLong
+                         withCompletionHandler:(void(^)(BOOL, NSArray*)) handler;
+
+@end
